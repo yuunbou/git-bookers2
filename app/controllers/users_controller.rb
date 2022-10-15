bc1b10b42b0b2aeeed2ad9ae71352a49db856753
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-10-15 15:37:52 +0000

    [Add]更新

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def uptate
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = "You have updated user successfully."
+      redirect_to book_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+  
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :introduction, length: { maximun: 50 }
+  
+  def get_profile_image(width, height)
+    unless purofile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit:[width, height]).processed
+  end
+
 end
